72d9151787c52933604b02c35087eb2417fa1864
htmlpluscss <79198889134@ya.ru>
2020-08-18 13:43:23 +0300

    main

--- src/js/menu.js
+++ src/js/menu.js
@@ -8,9 +8,57 @@
 
 	}
 
+	const wrapper = document.querySelector('.wrapper');
+
+
+	// открыть|закрыть меню
+
+	document.addEventListener('click', (e) => {
+
+		if(e.target.closest('.btn-menu-toggle') || e.target.classList.contains('.menu')) {
+
+			if(document.body.classList.contains('menu-open')){
+
+				setTimeout( () => {
+
+					window.scrollTo(0,MI.windowScrollOld);
+					wrapper.style.top = 0;
+
+				});
+
+			}
+			else {
+
+				MI.windowScrollOld = window.pageYOffset;
+				window.scrollTo(0,0);
+				wrapper.style.top = -MI.windowScrollOld + 'px';
+
+			}
+
+			document.body.classList.toggle('menu-open');
+			menu.classList.toggle('visuallyhidden');
+
+		}
+
+	});
+
+})(document.querySelector('.menu'));
+
+
+// меню каталога
+
+((menu) => {
+
+	"use strict";
+
+	if(!menu) {
+
+		return;
+
+	}
+
 	const level1 = menu.querySelectorAll('.menu-catalog__head'),
 		  level2 = menu.querySelectorAll('.nav__level2-item--parent');
-
 
 	// первый уровень
 
