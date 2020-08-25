d26c4a7f413f3231770365583d766e6370be0e51
htmlpluscss <79198889134@ya.ru>
2020-08-25 12:32:50 +0300

    menu catalog

--- src/js/menu.js
+++ src/js/menu.js
@@ -31,24 +31,19 @@
 })(document.querySelector('.menu'));
 
 
-// меню каталога
+// каталог на главной
 
 ((menu) => {
 
 	"use strict";
 
-	if(!menu) {
+	if(!menu.length) {
 
 		return;
 
 	}
 
-	const level1 = menu.querySelectorAll('.menu-catalog__head'),
-		  level2 = menu.querySelectorAll('.nav__level2-item--parent');
-
-	// первый уровень
-
-	Array.from(level1, el => {
+	Array.from(menu, el => {
 
 		el.addEventListener('click', evt => {
 
@@ -60,18 +55,144 @@
 
 	});
 
-	// второй уровень
+})(document.querySelectorAll('.main-catalog__head'));
 
-	Array.from(level2, el => {
+// меню каталога
 
-		el.querySelector('.nav__level2-link').addEventListener('click', evt => {
+((btns) => {
+
+	"use strict";
+
+	if(!btns.length) {
+
+		return;
+
+	}
+
+	const menu = document.querySelector('.menu-catalog'),
+		  level1 = menu.querySelectorAll('.menu-catalog__head--arrow'),
+		  level2 = menu.querySelectorAll('.menu-catalog__level2-link--arrow'),
+		  btnClose = menu.querySelector('.menu-catalog__close'),
+		  btnBack = menu.querySelector('.menu-catalog__back'),
+		  category = menu.querySelector('.menu-catalog__current-category'),
+		  categoryTextDefault = category.textContent;
+
+	let	level1Scroll = 0,
+		level2Scroll = 0,
+		level1Open = null,
+		level2Open = null;
+
+	// открыть меню
+
+	Array.from(btns, btn => btn.addEventListener('click', () => {
+
+		MI.windowScrollOld = window.pageYOffset;
+		window.scrollTo(0, 0);
+		document.body.classList.add('menu-catalog-open');
+		menu.classList.remove('visuallyhidden');
+
+	}));
+
+	// закрыть меню
+
+	btnClose.addEventListener('click', () => {
+
+		setTimeout( () => window.scrollTo(0, MI.windowScrollOld));
+
+		document.body.classList.remove('menu-catalog-open');
+		menu.classList.add('visuallyhidden');
+
+	});
+
+	// На уровень назад
+
+	btnBack.addEventListener('click', () => {
+
+		if(menu.classList.contains('is-level3')) {
+
+			menu.style.height = level1Open.nextElementSibling.scrollHeight + "px";
+
+			menu.classList.remove('is-level3');
+
+			Array.from(level2, elem => elem.parentNode.classList.remove('is-open'));
+
+			window.scrollTo(0, level2Scroll);
+
+			category.textContent = level1Open.textContent;
+
+		}
+		else if (menu.classList.contains('is-level2')) {
+
+			menu.style.height = menu.querySelector('.menu-catalog__inner').clientHeight + "px";
+
+			menu.classList.remove('is-level2');
+
+			Array.from(level1, elem => elem.classList.remove('is-open'));
+
+			window.scrollTo(0, level1Scroll);
+
+			btnBack.classList.add('hide');
+
+			category.textContent = categoryTextDefault;
+
+		}
+		else {
+
+			console.log('что-то не так');
+
+		}
+
+	});
+
+
+	// первый уровень
+
+	Array.from(level1, el => {
+
+		el.addEventListener('click', evt => {
 
 			evt.preventDefault();
 
-			el.classList.toggle('is-open');
+			level1Scroll = window.pageYOffset;
+
+			menu.style.height = el.nextElementSibling.scrollHeight + "px";
+
+			menu.classList.add('is-level2');
+
+			Array.from(level1, elem => elem.classList.toggle('is-open', elem === el));
+
+			level1Open = el;
+
+			btnBack.classList.remove('hide');
+
+			category.textContent = el.textContent;
 
 		});
 
 	});
 
-})(document.querySelector('.menu-catalog'));
+	// второй уровень
+
+	Array.from(level2, el => {
+
+		el.addEventListener('click', evt => {
+
+			evt.preventDefault();
+
+			level2Scroll = window.pageYOffset;
+
+			menu.style.height = el.parentNode.nextElementSibling.scrollHeight + "px";
+
+			menu.classList.add('is-level3');
+
+			Array.from(level2, elem => elem.parentNode.classList.toggle('is-open', elem === el));
+
+			level2Open = el;
+
+			category.textContent = el.textContent;
+
+		});
+
+	});
+
+})(document.querySelectorAll('.js-open-menu-catalog'));
