8e6eb20b0fb65b96e4043bcc03edb07a4d673153
htmlpluscss <79198889134@ya.ru>
2020-08-26 11:05:41 +0300

    schema

--- src/sw.js
+++ src/sw.js
@@ -1,13 +1,20 @@
-const CACHE = 'cache-only-v4';
+const CACHE = 'v1';
+
+console.log(CACHE);
+
+self.addEventListener('install', evt => {
+    console.log('!!! Установлен');
+    self.skipWaiting();
+});
 
 // событие activate
 self.addEventListener('activate', evt => {
-  console.log('activate', CACHE);
+  console.log('activate, удаляем старый кеш. ', CACHE);
   evt.waitUntil(
     caches.keys().then(keys => {
       return Promise.all(keys
         .filter(key =>  key !== CACHE)
-        .map(key => caches.delete(key))
+            .map(key => caches.delete(key))
       );
     })
   );
@@ -17,38 +24,42 @@ self.addEventListener('activate', evt => {
 self.addEventListener('fetch', event => {
   const url = new URL(event.request.url);
   const method = event.request.method;
-  console.log(CACHE);
   // при каких условиях используем кеш
   if (
         method === 'GET' &&
         url.origin === self.location.origin &&
-/*        /localhost"/i.test(url.pathname) === false &&*/
+        /localhost"/i.test(url.pathname) === false &&
         /admin/i.test(url.pathname) === false &&
         /browser-sync/i.test(url.pathname) === false &&
         (
-/*            /js/i.test(url.pathname) ||
-            /css/i.test(url.pathname) ||
-            /img/i.test(url.pathname) ||*/
-            /fonts/i.test(url.pathname)
+            /images/i.test(url.pathname) ||
+            /templates/i.test(url.pathname)
         )
     ) {
-    console.log(url.pathname);
 
-    // Мы используем `respondWith()`, чтобы мгновенно ответить без ожидания ответа с сервера.
-    event.respondWith(fromCache(event.request));
-    // `waitUntil()` нужен, чтобы предотвратить прекращение работы worker'a до того как кэш обновиться.
-    event.waitUntil(update(event.request));
+      console.log(CACHE, url.pathname);
+
+      // Мы используем `respondWith()`, чтобы мгновенно ответить без ожидания ответа с сервера.
+      event.respondWith(fromCache(event.request));
+      // `waitUntil()` нужен, чтобы предотвратить прекращение работы worker'a до того как кэш обновиться.
+      event.waitUntil(update(event.request));
+
 
     }
 
 });
 
 const fromCache = request => {
-    return caches.open(CACHE).then((cache) =>
-        cache.match(request).then((matching) =>
-            matching || Promise.reject('no-match')
-        ));
+    return caches.match(request).then((resp) => {
+      return resp || fetch(request).then((response) => {
+        return caches.open(CACHE).then((cache) => {
+          cache.put(request, response.clone());
+          return response;
+        });
+      });
+    });
 }
+
 
 const update = request => {
     return caches.open(CACHE).then((cache) =>
